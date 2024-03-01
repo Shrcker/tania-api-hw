a855c4d3e814ec831f16efca06b6400b87c65087
Tania <tanner.shirkey@gmail.com>
2024-03-01 16:25:34 -0500

    Added answer check & quiz advancement

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,40 @@
-var buttonA = document.querySelector('#choiceA');
-var buttonB = document.querySelector('#choiceB');
-var buttonC = document.querySelector('#choiceC');
-var questionForm = document.querySelectorAll('#form');
-var submitButton = document.querySelector('#submitBtn');
+const buttonA = document.querySelector('#choiceA');
+const buttonB = document.querySelector('#choiceB');
+const buttonC = document.querySelector('#choiceC');
+const questionForm = document.querySelectorAll('#form');
+const submitButton = document.querySelector('#submitBtn');
+const text = document.querySelector('legend');
+
+const answerList = 'abc'.split('');
+const savedAnswers = [];
+var answerIs = 0; // Index for answerList[]
 
 function submit(event) {
     event.preventDefault();
-    var selectedAnswer = document.querySelector('input[name="choices"]:checked');
-    console.log(selectedAnswer.value);
-    selectedAnswer.checked = false;
+    var userAnswer = document.querySelector('input[name="choices"]:checked');
+    // Returns value of any checked radio
+    if (userAnswer.value === answerList[answerIs]) { // Basic If check for if answer is "right"
+        text.textContent = 'Correct!';
+        savedAnswers.push(userAnswer.value);
+        advanceCheck();
+    } else if (userAnswer.value !== answerList[answerIs]){
+        text.textContent = 'Wrong!';
+        savedAnswers.push(userAnswer.value);
+        advanceCheck();
+    } else { 
+        text.textContent = "Please select an answer";
+    }
+    userAnswer.checked = false;
+    console.log(answerList[answerIs]); //debug to monitor which question quiz is on
+}
+
+function advanceCheck() { // Advances questions and recognizes when user reaches the end of the quiz
+    if (answerIs === answerList.length - 1) {
+        answerIs = 0;
+        console.log('Quiz has reset');
+        } else {
+            answerIs++;
+        }
 }
 
 submitButton.addEventListener("click", submit);
